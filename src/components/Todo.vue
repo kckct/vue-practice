e0107f0d5d6e8598f5d6ba7458f2e3d90b9904c7
paultseng <Paul.Tseng@senao.com.tw>
2018-08-29 11:55:30 +0800

    加入Todo

---
+++ src/components/Todo.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodo">
+        <button @click="addTodo">add</button>
+        <ul>
+          <li v-for="(todo, index) in todos" :key="index" @click="removeTodo">
+            {{ todo.title }}
+          </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [{ title: "a" }, { title: "b" }],
+      newTodo: ""
+    };
+  },
+
+  methods: {
+    addTodo() {
+      var value = this.newTodo && this.newTodo.trim();
+      if (!value) {
+        return;
+      }
+      this.todos.push({
+        title: value
+      });
+      this.newTodo = "";
+    },
+    removeTodo(todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1);
+    }
+  }
+};
+</script>
