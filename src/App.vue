de3f232383e5314e239db2b2e51228bf0f940558
paultseng <Paul.Tseng@senao.com.tw>
2018-09-06 15:51:49 +0800

    連動式下拉選單v2

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,79 @@
 <template>
   <div id="app">
-    <Dropdown />
+    <MyDropdown :options="cities" @selected-change="changeCity"></MyDropdown>
+    <MyDropdown :options="areas" @selected-change="changeArea"></MyDropdown>
+    <span>{{ zip }}</span>
   </div>
 </template>
 
 <script>
-import Dropdown from "@/components/Dropdown";
+import MyDropdown from "@/components/MyDropdown";
+
+const cities = [
+  {
+    name: "基隆市",
+    areas: [
+      { name: "仁愛區", zip: "200" },
+      { name: "信義區", zip: "201" },
+      { name: "中正區", zip: "202" },
+      { name: "中山區", zip: "203" },
+      { name: "安樂區", zip: "204" },
+      { name: "暖暖區", zip: "205" },
+      { name: "七堵區", zip: "206" }
+    ]
+  },
+  {
+    name: "台北市",
+    areas: [
+      { name: "中正區", zip: "300" },
+      { name: "大同區", zip: "301" },
+      { name: "中山區", zip: "302" },
+      { name: "松山區", zip: "303" },
+      { name: "大安區", zip: "304" },
+      { name: "萬華區", zip: "305" },
+      { name: "信義區", zip: "306" },
+      { name: "士林區", zip: "307" },
+      { name: "北投區", zip: "308" },
+      { name: "內湖區", zip: "309" },
+      { name: "南港區", zip: "310" },
+      { name: "文山區", zip: "311" }
+    ]
+  },
+  {
+    name: "新竹市",
+    areas: [{ name: "新竹市", zip: "400" }]
+  }
+];
 
 export default {
   name: "app",
+
   components: {
-    Dropdown
+    MyDropdown
+  },
+
+  data() {
+    return {
+      zip: "200",
+      cities: cities,
+      areas: cities[0].areas
+    };
+  },
+
+  methods: {
+    changeCity(newCity) {
+      this.areas = this.cities.find(function(city) {
+        return newCity === city.name;
+      }).areas;
+
+      this.zip = this.areas[0].zip;
+    },
+
+    changeArea(newArea) {
+      this.zip = this.areas.find(function(area) {
+        return newArea === area.name;
+      }).zip;
+    }
   }
 };
 </script>
---
+++ src/components/MyDropdown.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <select v-on:change="selectedChange">
+      <option v-for="option in options" v-bind:value="option.name">
+        {{ option.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["options", "selected"],
+
+  methods: {
+    selectedChange(event) {
+      this.$emit("selected-change", event.target.value);
+    }
+  }
+};
+</script>
