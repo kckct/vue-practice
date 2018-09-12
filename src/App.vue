017350d07041722f2059f84849482188b9a5ec24
paultseng <Paul.Tseng@senao.com.tw>
2018-09-12 13:26:35 +0800

    資料改call api

--- src/App.vue
+++ src/App.vue
@@ -7,43 +7,9 @@
 </template>
 
 <script>
+import axios from "axios";
+import _ from "lodash";
 import MyDropdown from "@/components/MyDropdown";
-
-const cities = [
-  {
-    name: "基隆市",
-    areas: [
-      { name: "仁愛區", zip: "200" },
-      { name: "信義區", zip: "201" },
-      { name: "中正區", zip: "202" },
-      { name: "中山區", zip: "203" },
-      { name: "安樂區", zip: "204" },
-      { name: "暖暖區", zip: "205" },
-      { name: "七堵區", zip: "206" }
-    ]
-  },
-  {
-    name: "台北市",
-    areas: [
-      { name: "中正區", zip: "300" },
-      { name: "大同區", zip: "301" },
-      { name: "中山區", zip: "302" },
-      { name: "松山區", zip: "303" },
-      { name: "大安區", zip: "304" },
-      { name: "萬華區", zip: "305" },
-      { name: "信義區", zip: "306" },
-      { name: "士林區", zip: "307" },
-      { name: "北投區", zip: "308" },
-      { name: "內湖區", zip: "309" },
-      { name: "南港區", zip: "310" },
-      { name: "文山區", zip: "311" }
-    ]
-  },
-  {
-    name: "新竹市",
-    areas: [{ name: "新竹市", zip: "400" }]
-  }
-];
 
 export default {
   name: "app",
@@ -54,18 +20,33 @@ export default {
 
   data() {
     return {
-      cities,
+      cities: [],
       cityIndex: 0,
       areaIndex: 0
     };
   },
 
+  mounted() {
+    axios
+      .get("http://localhost:8081/api")
+      .then(response => (this.cities = response.data))
+      .catch(e => console.log(e));
+  },
+
   computed: {
     areas() {
-      return cities[this.cityIndex].areas;
+      if (_.isEmpty(this.cities)) {
+        return [];
+      }
+
+      return this.cities[this.cityIndex].areas;
     },
 
     zip() {
+      if (_.isEmpty(this.areas)) {
+        return [];
+      }
+
       return this.areas[this.areaIndex].zip;
     }
   },
